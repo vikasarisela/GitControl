2e9c5f13a010413c5f12b9fd94be727ca51bfc84
AE.Vikas <vikasarisela@Vikass-MacBook-Air.local>
2018-04-09 17:45:40 +0530

    Initial Commit

---
+++ GitControl/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitControl
+//
+//  Created by AE.Vikas on 4/9/18.
+//  Copyright © 2018 Vikas Arisela. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
